0d7f689253ec4d693c72d646d7d990e9da7dc91a
Egor Gorbachev <7gorbachevm@gmail.com>
2021-06-24 13:23:21 +0300

    WIP generic model store & generic crud api

---
+++ store/model-api.ts
@@ -0,0 +1,23 @@
+import { AxiosInstance } from 'axios';
+
+type HasId = { id: string };
+
+export class ModelApi<Model extends HasId> {
+  constructor(private client: AxiosInstance, private url: string) {}
+
+  create(body: object) {
+    return this.client.post<Model>(this.url, body).then((response) => response.data);
+  }
+
+  update(id: string, body: object) {
+    return this.client.put<Model>(`${this.url}/${id}`, body).then((response) => response.data);
+  }
+
+  delete(id: string) {
+    return this.client.delete<unknown>(`${this.url}/${id}`).then((response) => response.data);
+  }
+
+  fetchOne(id: string) {
+    return this.client.get<Model>(`${this.url}/${id}`).then((response) => response.data);
+  }
+}
---
+++ store/model-page-store.ts
@@ -0,0 +1,83 @@
+import { Notificator } from '../notificator/notificator';
+import { action, makeAutoObservable } from 'mobx';
+import { handleFormSubmit } from '../final-form/handle-form-submit';
+import { v4 } from 'uuid';
+import { RouterStore } from '@superwf/mobx-react-router';
+import { assert } from 'ts-essentials';
+import { Translator } from '../intl/translator';
+
+type HasId = { id: string };
+
+type CrudApi<Model> = {
+  fetchOne: (modelId: string) => Promise<Model>;
+  create: (body: object) => Promise<Model>;
+  update: (modelId: string, body: object) => Promise<Model>;
+};
+
+type FormMapper<Model, Form> = {
+  mapModelToForm: (model: Model) => Form;
+};
+
+export class ModelPageStore<Model extends HasId, Form extends object> {
+  model?: Model;
+  isModelLoading = false;
+
+  constructor(
+    public crudApi: CrudApi<Model>,
+    public formMapper: FormMapper<Model, Form>,
+    public notificator: Notificator,
+    public routerStore: RouterStore,
+    public translator: Translator
+  ) {
+    makeAutoObservable(this, {
+      crudApi: false,
+      formMapper: false,
+      notificator: false,
+      routerStore: false,
+      translator: false,
+    });
+  }
+
+  loadModel(id: string) {
+    if (id === 'new') {
+    } else {
+      this.isModelLoading = true;
+      this.crudApi
+        .fetchOne(id)
+        .then(
+          action((model) => {
+            this.model = model;
+          })
+        )
+        .finally(action(() => (this.isModelLoading = false)));
+    }
+  }
+
+  get formInitialValues(): Partial<Form> {
+    if (!this.model) {
+      return {};
+    }
+    return this.formMapper.mapModelToForm(this.model);
+  }
+
+  submitForm = async (form: Form) => {
+    if (this.model) {
+      const result = await handleFormSubmit(this.crudApi.update(this.model.id, form));
+      if (result.errors) {
+        return result.errors;
+      }
+      this.notificator.success(this.translator.translate('riveradmin.data-saved'));
+    } else {
+      const modelCreateInput = { id: v4(), ...form };
+      const result = await handleFormSubmit(this.crudApi.create(modelCreateInput));
+      if (result.errors) {
+        return result.errors;
+      }
+      const regexResult = /\/([^\/]+)\//.exec(this.routerStore.location.pathname);
+      assert(regexResult);
+      const pathWithoutModelId = regexResult[1];
+      this.routerStore.push(`${pathWithoutModelId}/${modelCreateInput.id}`);
+      this.notificator.success(this.translator.translate('riveradmin.data-saved'));
+    }
+  };
+}
