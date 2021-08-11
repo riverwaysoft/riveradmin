b9d74d991ce0cccf564603082705b172caad3751
Egor Gorbachev <7gorbachevm@gmail.com>
2021-08-11 19:42:38 +0300

    Clickable label for checkbox adapter

--- components/ui/checkbox-adapter.tsx
+++ components/ui/checkbox-adapter.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { FieldRenderProps } from 'react-final-form';
 import { Form } from 'react-bootstrap';
+import { css } from '@emotion/css/macro';
 
 type Props<T> = FieldRenderProps<T, HTMLInputElement>;
 
 export const CheckboxAdapter = <T extends string | number>(props: Props<T>) => {
   return (
     <Form.Group>
-      <Form.Check {...props.input} type="checkbox" label={props.label} />
+      <label
+        className={css`
+          padding: 0;
+          display: flex;
+        `}
+      >
+        <Form.Check {...props.input} type="checkbox" />
+        {props.label}
+      </label>
     </Form.Group>
   );
 };
