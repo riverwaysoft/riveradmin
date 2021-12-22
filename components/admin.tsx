1e3aca3a085f08c47b8ccd5d517a367b36b8d95e
Gorbachev Egor <7gorbachevm@gmail.com>
2021-12-22 16:55:58 +0300

    Start adding more comments

--- components/admin.tsx
+++ components/admin.tsx
@@ -5,12 +5,23 @@ import { RiverAdminRootStore } from '../store/river-admin-root-store';
 import { StoreContext as RiverAdminRootStoreContext } from '../store/use-riveradmin-store';
 import { Layout } from './layout/layout';
 
+// This is admin route configuration
 export type MenuRoutes = {
+  // The path of the route
   [key in string]: {
+    // The title of the route. It will be displayed as tab title in browser + as <h1> header on a page
     title: string;
+    // Should this route be added to the menu list?
     menu?: boolean;
+    // Pass query params for the route
+    // For example you may want to pass default sorting params for a page
+    // Usage: ['/users', { query: { createdAt: 'desc' } }] will be transformed to /users?createdAt=desc
     query?: object;
-  } & Pick<RouteProps, 'component' | 'exact'>;
+    // A component to render for this route. It is the same as React Router's 'component' prop for a <Route/>
+    component: RouteProps['component'];
+    // It is the same as React Router's 'exact' prop for a <Route/>
+    exact?: RouteProps['exact'];
+  };
 };
 
 type Props = {
