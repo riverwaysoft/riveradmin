3530ca6eede21c76834717a1eb0b7325f54f97aa
Gorbachev Egor <7gorbachevm@gmail.com>
2022-01-06 18:23:13 +0300

    Fix react warning

--- components/grid/filters/admin-calendar/prev-icon.tsx
+++ components/grid/filters/admin-calendar/prev-icon.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export const PrevIcon = () => {
   return <svg width="9" height="14" viewBox="0 0 9 14" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M7.02941 1.00001L1.37256 6.65686L7.02941 12.3137" stroke="#333333" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
