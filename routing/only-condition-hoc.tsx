16e4dbe2410f81e03abb6a58133706e0d4cc5521
Gorbachev Egor <7gorbachevm@gmail.com>
2022-01-06 16:01:31 +0300

    Fix wrong import

--- routing/only-condition-hoc.tsx
+++ routing/only-condition-hoc.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { observer } from 'mobx-react-lite';
-import { useAdminStore } from '../../admin/stores/use-admin-store';
 import { AdminAuthStore } from '../store/admin-auth-store';
+import {useRiverAdminStore} from "../store/use-riveradmin-store";
 
 export const onlyConditionHoc = (
   WrappedComponent: React.ComponentType<{}>,
   callback: (adminAuthStore: AdminAuthStore) => boolean
 ) => {
   return observer(() => {
-    const { authStore } = useAdminStore();
+    const { authStore } = useRiverAdminStore();
     const isAllowed = callback(authStore);
     if (isAllowed) {
       return <WrappedComponent />;
