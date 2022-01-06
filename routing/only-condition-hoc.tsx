84a649c610f1de53c2674f425fe2eac90fdf392b
Gorbachev Egor <7gorbachevm@gmail.com>
2022-01-06 18:16:23 +0300

    Fix formatting

--- routing/only-condition-hoc.tsx
+++ routing/only-condition-hoc.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { observer } from 'mobx-react-lite';
 import { AdminAuthStore } from '../store/admin-auth-store';
-import {useRiverAdminStore} from "../store/use-riveradmin-store";
+import { useRiverAdminStore } from '../store/use-riveradmin-store';
 
 export const onlyConditionHoc = (
   WrappedComponent: React.ComponentType<{}>,
