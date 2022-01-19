333beb9d7f18031387665b611330cba4c77a2b90
Gorbachev Egor <7gorbachevm@gmail.com>
2022-01-19 17:26:46 +0300

    Drop ActionsProps<T>

--- components/grid/column/actions-column.tsx
+++ components/grid/column/actions-column.tsx
@@ -1,8 +1,6 @@
 import { css } from '@emotion/css';
 import { observer } from 'mobx-react-lite';
 import React, { ReactNode } from 'react';
-import { HasId } from '../../../model/hydra';
-import { ListStore } from '../../../store/list-store';
 
 export const ActionsColumn = observer((props: { children: ReactNode }) => {
   return <div className={css({ display: 'flex', gap: 16 })}>{props.children}</div>;
