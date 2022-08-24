15f06659963c13ccc306db957c6bd963a375e4bd
Gorbachev Egor <7gorbachevm@gmail.com>
2022-08-24 17:18:57 +0300

    Add isValidEnum TS helper

---
+++ typescript/is-valid-enum.ts
@@ -0,0 +1,8 @@
+export function isValidEnumValue<T extends object>(
+  param: unknown,
+  environments: T
+): param is T[keyof T] {
+  return !(
+    typeof param !== 'string' || !Object.values(environments).includes(param)
+  );
+}
