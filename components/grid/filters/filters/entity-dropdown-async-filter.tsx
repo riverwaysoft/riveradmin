3e1da1b4ad6c942120d214e310afa892c6da34b1
Gorbachev Egor <7gorbachevm@gmail.com>
2022-09-09 14:53:54 +0300

    Generic async dropdown field

--- components/grid/filters/filters/entity-dropdown-async-filter.tsx
+++ components/grid/filters/filters/entity-dropdown-async-filter.tsx
@@ -6,22 +6,24 @@ import { debounce } from 'lodash';
 import AsyncSelect from 'react-select/async';
 import { trimRight } from './trim-right';
 
-type Props = {
+type Props<T> = {
   // Final Form's field name
   fieldName: string;
   // API endpoint where to fetch data list for the dropdown
   endpoint: string;
   // A human-readable value to display in dropdown (object property from the dropdown list)
   labelKey: string;
-  labelKeyComputed?: (model: any) => string;
+  labelKeyComputed?: (model: T) => string;
   // An entity prefix (Iri) that API platform requires to send when using SearchFilter
   // Usually the Iri prefix is not different from the endpoint property.
   // But if the endpoint is custom (for example /api/visible_users), then iri prefix should still point to an entity (for example /api/users/)
   iriPrefix: string;
+  isDisabled?: boolean;
+  filter?: (model: T) => boolean;
 };
 
-export const EntityDropdownAsyncFilter = (props: Props) => {
-  const { fieldName, labelKey, labelKeyComputed } = props;
+export const EntityDropdownAsyncFilter = <T extends any>(props: Props<T>) => {
+  const { fieldName, labelKey, labelKeyComputed, isDisabled, filter } = props;
   const iriPrefix = trimRight(props.iriPrefix, '/');
   const endpoint = trimRight(props.endpoint, '/');
   const [lastLabel, setLastLabel] = useState('');
@@ -33,7 +35,8 @@ export const EntityDropdownAsyncFilter = (props: Props) => {
           <AsyncSelect
             className={css({ minWidth: 250 })}
             cacheOptions
-            defaultOptions
+            defaultOptions={filter ? undefined : true}
+            isDisabled={isDisabled}
             onChange={debounce((e: any) => {
               input.onChange(e.value);
               // Fix stupid react-select issue: https://github.com/JedWatson/react-select/issues/3761
@@ -54,14 +57,16 @@ export const EntityDropdownAsyncFilter = (props: Props) => {
                     [labelKey]: inputValue,
                   },
                 })
-                .then((response) =>
-                  response.data['hydra:member'].map((model: any) => {
+                .then((response) => {
+                  const data = response.data['hydra:member'];
+                  const filteredData = filter ? data.filter(filter) : data;
+                  return filteredData.map((model: any) => {
                     return {
                       label: labelKeyComputed ? labelKeyComputed(model) : model[labelKey],
                       value: `${iriPrefix}/${model.id}`,
                     };
-                  })
-                );
+                  });
+                });
             }}
           />
         );
