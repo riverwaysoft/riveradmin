d0fe4d02c8086e6082ea99e5158982a7a6f1df6d
Gorbachev Egor <7gorbachevm@gmail.com>
2023-04-14 16:27:20 +0300

    Allow to override groupClassName

--- components/ui/checkbox-adapter.tsx
+++ components/ui/checkbox-adapter.tsx
@@ -8,7 +8,7 @@ type Props<T> = FieldRenderProps<T, HTMLInputElement>;
 
 export const CheckboxAdapter = <T extends string | number>(props: Props<T>) => {
   return (
-    <Form.Group>
+    <Form.Group className={props.groupClassName}>
       <label className={css({ display: 'flex', padding: 0 })}>
         <Form.Check {...props.input} type="checkbox" />
         {props.label}
