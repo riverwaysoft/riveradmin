c2664b694d7fe94f6d09c2ed9f46a9fcd57365be
Gorbachev Egor <7gorbachevm@gmail.com>
2023-04-15 15:14:09 +0300

    Props type extract

--- components/grid/table-simple.tsx
+++ components/grid/table-simple.tsx
@@ -6,71 +6,71 @@ import { Loader } from './loader';
 import { css } from '@emotion/css';
 import { useTranslate } from '../../store/use-translate';
 
-export const TableSimple = observer(
-  <T extends any>(props: {
-    models?: IPromiseBasedObservable<T[]> | T[];
-    isLoading?: boolean;
-    columns: {
-      label: string | React.ReactNode;
-      onClick?: (e: React.MouseEvent<HTMLElement>) => void;
-      render: (item: T) => React.ReactNode | string | null;
-    }[];
-    onRowClick?: (model: T) => void;
-  }) => {
-    const { models, columns, isLoading } = props;
-    const t = useTranslate();
+type Props<T extends any> = {
+  models?: IPromiseBasedObservable<T[]> | T[];
+  isLoading?: boolean;
+  columns: {
+    label: string | React.ReactNode;
+    onClick?: (e: React.MouseEvent<HTMLElement>) => void;
+    render: (item: T) => React.ReactNode | string | null;
+  }[];
+  onRowClick?: (model: T) => void;
+};
 
-    if (!models) {
-      return null;
-    }
+export const TableSimple = observer(<T extends any>(props: Props<T>) => {
+  const { models, columns, isLoading } = props;
+  const t = useTranslate();
 
-    if (isLoading) {
-      return <Loader />;
-    }
+  if (!models) {
+    return null;
+  }
 
-    if (!Array.isArray(models) && models.state === 'pending') {
-      return <Loader />;
-    }
+  if (isLoading) {
+    return <Loader />;
+  }
 
-    if (!Array.isArray(models) && models.state === 'rejected') {
-      return null;
-    }
+  if (!Array.isArray(models) && models.state === 'pending') {
+    return <Loader />;
+  }
 
-    const value = Array.isArray(models) ? models : models.value;
+  if (!Array.isArray(models) && models.state === 'rejected') {
+    return null;
+  }
 
-    return (
-      <Table responsive>
-        <thead>
+  const value = Array.isArray(models) ? models : models.value;
+
+  return (
+    <Table responsive>
+      <thead>
+        <tr>
+          {columns.map((column, i) => (
+            <td key={i}>{column.label}</td>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {value.length === 0 && (
           <tr>
-            {columns.map((column, i) => (
-              <td key={i}>{column.label}</td>
+            <td className={css({ width: '100%', textAlign: 'center' })} colSpan={columns.length}>
+              <p>{t('No items matched your search')}</p>
+            </td>
+          </tr>
+        )}
+        {value.map((model, i) => (
+          <tr key={i}>
+            {columns.map((column, j) => (
+              <td
+                key={j}
+                onClick={() => {
+                  props.onRowClick?.(model);
+                }}
+              >
+                {column.render(model)}
+              </td>
             ))}
           </tr>
-        </thead>
-        <tbody>
-          {value.length === 0 && (
-            <tr>
-              <td className={css({ width: '100%', textAlign: 'center' })} colSpan={columns.length}>
-                <p>{t('No items matched your search')}</p>
-              </td>
-            </tr>
-          )}
-          {value.map((model, i) => (
-            <tr key={i}>
-              {columns.map((column, j) => (
-                <td
-                  key={j}
-                  onClick={() => {
-                    props.onRowClick?.(model);
-                  }}
-                >
-                  {column.render(model)}
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </Table>
-    );
-  }
-);
+        ))}
+      </tbody>
+    </Table>
+  );
+});
